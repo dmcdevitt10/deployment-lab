3bc4a8b90aa4bf6e450b11205ff7b62a72acf618
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-03-22 16:03:24 -0600

    change folder name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.use(express.static(`${__dirname}/client`))
+app.use(express.static(`${__dirname}/public`))
 
 app.listen(5500, () => {
     console.log('app is up on 5500')
